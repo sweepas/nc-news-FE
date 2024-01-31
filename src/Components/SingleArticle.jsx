131bf206364ead251ea7a1b8079e08ab1e623203
sweepas <47362063+sweepas@users.noreply.github.com>
2024-01-31 13:49:44 +0000

    styles

--- src/Components/SingleArticle.jsx
+++ src/Components/SingleArticle.jsx
@@ -26,6 +26,17 @@ function SingleArticle() {
         setError(error);
       });
   }, []);
+  function formatISODateTime(isoDate) {
+    const date = new Date(isoDate);
+    const options = {
+      year: "numeric",
+      month: "numeric",
+      day: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+    };
+    return date.toLocaleDateString(undefined, options);
+  }
 
   if (loading) {
     return <p>Loading...</p>;
@@ -39,7 +50,7 @@ function SingleArticle() {
       <div className="single-article-container ">
         <div className="single-article">
         <h2>{article.title}</h2>
-        <p>{article.created_at}</p>
+        <p>{formatISODateTime(article.created_at)}</p>
         <p>by {article.author}</p>
         <img className="article-img"
           src={article.article_img_url}
